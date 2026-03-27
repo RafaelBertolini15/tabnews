85d9841a88a9d5932bfb6dbf67e0cc759eaefafa
Rafael Henrique Bertolini <rafaelhbertolini@gmail.com>
2026-03-27 15:49:49 +0000

    adds api migrations

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,41 @@
+import migrationRunner from 'node-pg-migrate';
+import { join } from "node:path";
+import database from 'infra/database.js';
+
+export default async function migrations(request, response) {
+
+  const dbClient = await database.getNewClient();
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations"
+  }
+
+  if(request.method === 'GET'){
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+  response.status(200).json(pendingMigrations);
+}
+
+  if (request.method === 'POST'){
+    const migretedMigrations = await migrationRunner({
+    ...defaultMigrationsOptions,
+    dryRun: false,
+  });
+  
+  await dbClient.end();
+
+  if (migretedMigrations.length > 0) {
+     response.status(201).json(migretedMigrations);
+  }
+
+  response.status(200).json(migretedMigrations);
+}
+  return response.status(405).end();
+}
+
+
+  
