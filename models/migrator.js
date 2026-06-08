fc08b217b1d5c797597d459c4b4aa6938439e35c
Rafael Henrique Bertolini <rafaelhbertolini@gmail.com>
2026-06-08 13:20:07 +0000

    feat: hide migrator logs

--- models/migrator.js
+++ models/migrator.js
@@ -6,7 +6,7 @@ const defaultgetHandlerOptions = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
